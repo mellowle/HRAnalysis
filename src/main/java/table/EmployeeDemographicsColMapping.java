686440c80c524b72d727bf86297689d24ec1c4ba
Le, Le [MEDCN] <lle23@its.jnj.com>
2019-07-03 13:17:32 +0800

    updated

--- src/main/java/table/EmployeeDemographicsColMapping.java
+++ src/main/java/table/EmployeeDemographicsColMapping.java
@@ -1,4 +1,13 @@
 package table;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class EmployeeDemographicsColMapping {
+    public final static String TABLE_NAME = "employee_demographics";
+    public static Map<String, String> COLUMN_MAPPING;
+    static {
+        COLUMN_MAPPING = new HashMap<>();
+        COLUMN_MAPPING.put("EXCEL_HEADER", "TABLE_HEADER");
+    }
 }
