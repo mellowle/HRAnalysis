7eca38388547686cbfbb87143c8f3fcac8048d7b
Le, Le [MEDCN] <lle23@its.jnj.com>
2019-07-03 16:22:09 +0800

    updated

---
+++ src/main/java/table/TalentCardColMapping.java
@@ -0,0 +1,17 @@
+package table;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TalentCardColMapping {
+    public final static String TABLE_NAME = "talent_card";
+    public static Map<String, String> COLUMN_MAPPING;
+    static {
+        COLUMN_MAPPING = new HashMap<>();
+        COLUMN_MAPPING.put("wwid", "WWID");
+        COLUMN_MAPPING.put("number_of_jnj_roles", "NumberOfJnJRoles");
+        COLUMN_MAPPING.put("average_time_of_jnj_roles", "AverageTimeOfJnJRole");
+        COLUMN_MAPPING.put("number_of_external_roles", "NumberOfExternalRoles");
+        COLUMN_MAPPING.put("average_time_of_external_roles", "AverageTimeOfExternalRoles");
+    }
+}
