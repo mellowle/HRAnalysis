bbe9cf4d6110df1c4763598dd0d451408997478b
Le, Le [MEDCN] <lle23@its.jnj.com>
2019-07-04 17:54:34 +0800

    updated

---
+++ src/main/java/table/UnstructuredDataColMapping.java
@@ -0,0 +1,16 @@
+package table;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UnstructuredDataColMapping {
+    public final static String TABLE_NAME = "unstructured_data";
+    public static Map<String, String> COLUMN_MAPPING;
+
+    static {
+        COLUMN_MAPPING = new HashMap<>();
+        COLUMN_MAPPING.put("wwid", "wwid");
+        COLUMN_MAPPING.put("performanceComments", "2018 Year-end performance comments rating");
+        COLUMN_MAPPING.put("feedBack", "Stakeholder feedback rating");
+    }
+}
