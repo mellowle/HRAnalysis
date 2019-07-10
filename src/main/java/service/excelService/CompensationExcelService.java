6a7c634a4099692f8ab45f18c57a3224fd56be84
Le, Le [MEDCN] <lle23@its.jnj.com>
2019-07-10 14:42:09 +0800

    updated

--- src/main/java/service/excelService/CompensationExcelService.java
+++ src/main/java/service/excelService/CompensationExcelService.java
@@ -6,10 +6,11 @@ import excelMapping.CompensationColMapping;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class CompensationExcelService extends AbstractExcelService {
 
-    public CompensationExcelService(){
+    public CompensationExcelService() {
         this.COLUMN_MAPPING = CompensationColMapping.COLUMN_MAPPING;
         this.EXCEL_NAME = CompensationColMapping.EXCEL_NAME;
         this.TABLE_NAME = CompensationColMapping.TABLE_NAME;
@@ -20,18 +21,6 @@ public class CompensationExcelService extends AbstractExcelService {
         CompensationExcelService c = new CompensationExcelService();
         List<Compensation> results = c.getResults();
         System.out.println(results.size());
-
-        for (Compensation comp : results) {
-            if (comp.getBonus2016() == null && comp.getBonus2017() != null && comp.getBonus2018() != null) {
-                comp.setBonus2016(0.0);
-            }
-
-            if (comp.getBonus2016() == null && comp.getBonus2017() == null && comp.getBonus2018() != null) {
-                comp.setBonus2016(0.0);
-                comp.setBonus2017(0.0);
-            }
-        }
-
         Set<String> tmp = Sets.newHashSet();
         results.forEach(result->{
             tmp.add(result.getWwid());
@@ -43,6 +32,18 @@ public class CompensationExcelService extends AbstractExcelService {
     }
 
     public List<Compensation> getResults() throws Exception {
-        return sheet2Entities(Compensation.class);
+        List<Compensation> results = sheet2Entities(Compensation.class);
+        results = results.stream().filter(i->i.getWwid() != null).collect(Collectors.toList());
+        for (Compensation comp : results) {
+            if (comp.getBonus2016() == null && comp.getBonus2017() != null && comp.getBonus2018() != null) {
+                comp.setBonus2016(0.0);
+            }
+
+            if (comp.getBonus2016() == null && comp.getBonus2017() == null && comp.getBonus2018() != null) {
+                comp.setBonus2016(0.0);
+                comp.setBonus2017(0.0);
+            }
+        }
+        return results;
     }
 }
